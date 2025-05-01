7544d2212de08aace8ddfd4a7e925a1ec9a365a6
mriley04 <michael.riley918@gmail.com>
2025-04-30 22:48:15 -0400

    Update firebaseConfig.js
    Fix bugs to allow API

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDAov6yR3LkPyy5x_2ZFY4D87bMcMc2jnw",
   authDomain: "bucketlist-41856.firebaseapp.com",
   projectId: "bucketlist-41856",
-  storageBucket: "bucketlist-41856.firebasestorage.app",
+  storageBucket: "bucketlist-41856.appspot.com",
   messagingSenderId: "289122432817",
-  appId: "1:289122432817:web:25ee72ab12f99354542210",
-  measurementId: "G-876LDPBJ4D"
+  appId: "1:289122432817:web:25ee72ab12f99354542210"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
